04599ffc69723be8142f265dfe3defc72ad42f2d
Mark Bell <me@markashleybell.com>
2011-02-03 21:50:55 +0000

    Refactoring admin section

--- markashleybell.com.Web/Models/ArticleViewModel.cs
+++ markashleybell.com.Web/Models/ArticleViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
 
 namespace markashleybell.com.Web.Models
 {
@@ -14,8 +15,10 @@ namespace markashleybell.com.Web.Models
         public string Title { get; set; }
         public string Author { get; set; }
         public string Summary { get; set; }
+        [AllowHtml]
         public string SummaryHtml { get; set; }
         [Required]
+        [AllowHtml]
         public string Body { get; set; }
         public string BodyHtml { get; set; }
         public string Slug { get; set; }
--- markashleybell.com.Web/Scripts/comment.js
+++ markashleybell.com.Web/Scripts/comment.js
@@ -1,27 +1,23 @@
 ﻿$(document).ready(function () {
     $('#commentform').submit(function () {
 
-        return true;
-
-        var requiredFields = ['NewComment_AuthorName', 'NewComment_Email', 'NewComment_Body', 'z7sfd602nlwi'];
+        var requiredFields = ['Comment_AuthorName', 'Comment_Email', 'Comment_Body', 'z7sfd602nlwi'];
         var valid = true;
-        $(this).find('span.validationmsg').remove();
-        $(this).find('input').removeClass();
+        $(this).find('.field-validation-error').remove();
+        $(this).find('input, textarea').removeClass('input-validation-error');
         for (var x = 0; x < requiredFields.length; x++) {
             var msg = '';
             var field = $(this).find('[id=' + requiredFields[x] + ']');
             if (field.val() == '') { valid = false; field.addClass('invalid'); msg = 'Please fill in this field' }
-            else if (field.attr('id') == 'NewComment_Email' && !field.val().match(/^([0-9a-zA-Z]+[-._+&])*[0-9a-zA-Z]+@([-0-9a-zA-Z]+[.])+[a-zA-Z]{2,6}$/gi)) {
+            else if (field.attr('id') == 'Comment_Email' && !field.val().match(/^([0-9a-zA-Z]+[-._+&])*[0-9a-zA-Z]+@([-0-9a-zA-Z]+[.])+[a-zA-Z]{2,6}$/gi)) {
                 valid = false;
-                field.addClass('invalid');
                 msg = 'Invalid email address';
             }
             else if (field.attr('name') == 'z7sfd602nlwi' && isNaN(parseInt(field.val()))) {
                 valid = false;
-                field.addClass('invalid');
                 msg = 'Hint: the answer is 16...';
             }
-            if (msg != '') field.after('<span id="' + field.attr('name') + '-validationmsg" class="validationmsg">' + msg + '</span>')
+            if (msg != '') field.addClass('input-validation-error').after('<span id="' + field.attr('name') + '-validationmsg" class="field-validation-error">' + msg + '</span>')
         }
         return valid;
     });
