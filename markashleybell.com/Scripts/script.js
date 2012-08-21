5bf6de9225a830f1a9662d527fbd65a92d61847a
Mark Bell <me@markashleybell.com>
2012-08-21 07:11:55 +0100

    Add DevMode config flag, rewrite email replacer so it copes with missing element

--- markashleybell.com/Scripts/script.js
+++ markashleybell.com/Scripts/script.js
@@ -64,22 +64,19 @@ $(function () {
         }
     });
 });﻿$(function () {
-    
-    var l = document.getElementById('m'), p = document.getElementById('e'), t = '', a = null;
-    
-    var v = '109#101#64#109#97#114#107#97#115#104#108#101#121#98#101#108#108#46#99#111#109'.split('#');
 
-    for (var x = 0; x < v.length; x++)
-        t += String.fromCharCode(parseInt(v[x], 10));
+    var e = $('#e');
 
-    a = document.createElement('a');
+    if (e.length) {
 
-    a.href = String.fromCharCode(109, 97, 105, 108, 116, 111, 58) + t;
+        var l = document.getElementById('m'), p = document.getElementById('e'), t = '', a = null;
 
-    var n = document.createTextNode(t);
+        var v = '109#101#64#109#97#114#107#97#115#104#108#101#121#98#101#108#108#46#99#111#109'.split('#');
 
-    a.appendChild(n);
+        for (var x = 0; x < v.length; x++)
+            t += String.fromCharCode(parseInt(v[x], 10));
 
-    p.replaceChild(a, p.firstChild);
+        e.empty().append('<a href="' + String.fromCharCode(109, 97, 105, 108, 116, 111, 58) + t + '">' + t + '</a>');
+    }
 
 });
