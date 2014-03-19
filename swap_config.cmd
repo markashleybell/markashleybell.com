5e6147861fc824adc0d86920f5fc62235b22d7d8
Mark Bell <me@markashleybell.com>
2014-03-19 07:53:37 +0000

    Fix config batch file

--- swap_config.cmd
+++ swap_config.cmd
@@ -1,21 +1,19 @@
 @ECHO OFF
 IF "%1"=="local" (
     IF EXIST "config.cfg.local" (
-        ren config.cfg config.tmp
-        ren config.cfg.local config.cfg
-        ren config.tmp config.cfg.live
+        del /q config.cfg
+        copy config.cfg.local config.cfg
         ECHO Switched to local config
     ) ELSE (
-        ECHO Already using local config
+        ECHO Local config file missing
     )
 ) ELSE IF "%1"=="live" (
     IF EXIST "config.cfg.live" (
-        ren config.cfg config.tmp
-        ren config.cfg.live config.cfg
-        ren config.tmp config.cfg.local
+        del /q config.cfg
+        copy config.cfg.live config.cfg
         ECHO Switched to live config
     ) ELSE (
-        ECHO Already using live config
+        ECHO Live config file missing
     )
 ) ELSE (
     ECHO Specify 'live' or 'local'
