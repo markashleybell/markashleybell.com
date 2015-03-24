e25d42f9ca7e4ca9f0b68528f02cd0917b3a4072
Mark Bell <me@markb.co.uk>
2015-03-24 09:06:12 +0000

    Update email address

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ $(function () {
     var e = $('#e');
     if (e.length) {
         var l = document.getElementById('m'), p = document.getElementById('e'), t = '', a = null;
-        var v = '109#101#64#109#97#114#107#97#115#104#108#101#121#98#101#108#108#46#99#111#109'.split('#');
+        var v = '109#101#64#109#97#114#107#98#46#99#111#46#117#107'.split('#');
 
         for (var x = 0; x < v.length; x++)
             t += String.fromCharCode(parseInt(v[x], 10));
