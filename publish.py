742c17d58d952615740330e09bc53f653aa57792
Mark Bell <me@markb.co.uk>
2018-01-28 19:58:43 +0000

    Use consistent quote style and Python 3 string formatting

--- publish.py
+++ publish.py
@@ -14,35 +14,35 @@ from rss import create_rss_xml
 def get_config():
     """Load configuration from files."""
     parser = configparser.RawConfigParser()
-    parser.read('config.cfg')
-    parser.read('versions.cfg')
+    parser.read("config.cfg")
+    parser.read("versions.cfg")
 
     return {
-        'hostname': parser.get('Site', 'hostname'),
-        'cdn1': parser.get('Site', 'cdn1'),
-        'cdn2': parser.get('Site', 'cdn2'),
-        'analytics_id': parser.get('Site', 'analytics_id'),
-        'disqus_id': parser.get('Site', 'disqus_id'),
-        'asset_version': parser.get('Versions', 'asset_version'),
-        'output_folder': 'public'
+        "hostname": parser.get("Site", "hostname"),
+        "cdn1": parser.get("Site", "cdn1"),
+        "cdn2": parser.get("Site", "cdn2"),
+        "analytics_id": parser.get("Site", "analytics_id"),
+        "disqus_id": parser.get("Site", "disqus_id"),
+        "asset_version": parser.get("Versions", "asset_version"),
+        "output_folder": "public"
     }
 
 def load_templates():
     """Load Jinja templates."""
-    environment = Environment(loader=FileSystemLoader('templates/'))
+    environment = Environment(loader=FileSystemLoader("templates/"))
 
     return {
-        'index': environment.get_template('index.html'),
-        'post': environment.get_template('post.html')
+        "index": environment.get_template("index.html"),
+        "post": environment.get_template("post.html")
     }
 
 def parse_iso8601_date(date_string):
     """Parse an ISO 8601 date string and return a date object."""
-    return datetime.datetime.strptime(date_string, '%Y-%m-%d %H:%M')
+    return datetime.datetime.strptime(date_string, "%Y-%m-%d %H:%M")
 
 def is_static_page(page_metadata):
     """Returns true if the page is a static page (not a post)."""
-    return page_metadata['pagetype'] == 'static'
+    return page_metadata["pagetype"] == "static"
 
 def get_header_string(header_regex, content):
     """Get the value retrieved from the specified header."""
@@ -56,40 +56,40 @@ def get_header_date(header_regex, content):
 
 def strip_post_metadata(header_regex, content):
     """Strip the headers from the content of a post."""
-    no_metadata = header_regex['title'].sub('', content)
-    no_metadata = header_regex['published'].sub('', no_metadata)
-    no_metadata = header_regex['updated'].sub('', no_metadata)
-    no_metadata = header_regex['abstract'].sub('', no_metadata)
-    no_metadata = header_regex['pagetype'].sub('', no_metadata)
-    no_metadata = header_regex['thumbnail'].sub('', no_metadata)
+    no_metadata = header_regex["title"].sub("", content)
+    no_metadata = header_regex["published"].sub("", no_metadata)
+    no_metadata = header_regex["updated"].sub("", no_metadata)
+    no_metadata = header_regex["abstract"].sub("", no_metadata)
+    no_metadata = header_regex["pagetype"].sub("", no_metadata)
+    no_metadata = header_regex["thumbnail"].sub("", no_metadata)
     return no_metadata
 
 def parse_page_data(header_regex, content, source_filename, output_filename, cdn_url):
     """Parse headers (date and title) from page file content."""
-    abstract_text = get_header_string(header_regex['abstract'], content)
+    abstract_text = get_header_string(header_regex["abstract"], content)
 
-    more_link = ' <a class="more-link" href="' + output_filename + '">&rarr;</a>'
+    more_link = " <a class=\"more-link\" href=\"{0}\">&rarr;</a>".format(output_filename)
 
     content_no_metadata = strip_post_metadata(header_regex, content)
     body = re.sub(r"(\$\{cdn2\})", cdn_url, content_no_metadata)
 
     return {
-        'title': get_header_string(header_regex['title'], content),
-        'published': get_header_date(header_regex['published'], content),
-        'updated': get_header_date(header_regex['updated'], content),
-        'body': markdown.markdown(body, extensions=['extra', 'codehilite']),
-        'abstract': markdown.markdown(abstract_text + more_link) if abstract_text else None,
-        'abstract_plain': abstract_text if abstract_text else None,
-        'abstract_nolink': markdown.markdown(abstract_text) if abstract_text else None,
-        'pagetype': get_header_string(header_regex['pagetype'], content),
-        'thumbnail': get_header_string(header_regex['thumbnail'], content),
-        'source_filename': source_filename,
-        'output_filename': output_filename
+        "title": get_header_string(header_regex["title"], content),
+        "published": get_header_date(header_regex["published"], content),
+        "updated": get_header_date(header_regex["updated"], content),
+        "body": markdown.markdown(body, extensions=["extra", "codehilite"]),
+        "abstract": markdown.markdown(abstract_text + more_link) if abstract_text else None,
+        "abstract_plain": abstract_text if abstract_text else None,
+        "abstract_nolink": markdown.markdown(abstract_text) if abstract_text else None,
+        "pagetype": get_header_string(header_regex["pagetype"], content),
+        "thumbnail": get_header_string(header_regex["thumbnail"], content),
+        "source_filename": source_filename,
+        "output_filename": output_filename
     }
 
 def write_file_utf8(output_filename, content):
     """Write content to the specified file with UTF-8 encoding."""
-    codec = codecs.open(output_filename, 'w', 'utf-8')
+    codec = codecs.open(output_filename, "w", "utf-8")
     codec.write(content)
     codec.close()
 
@@ -101,11 +101,11 @@ def delete_files(file_spec):
 def concatenate_files(file_spec, output_file_name):
     """Concatenate all files matching file_spec glob pattern."""
     files = [f for f in glob.glob(file_spec)]
-    with open(output_file_name, 'w') as fout:
+    with open(output_file_name, "w") as fout:
         for line in fileinput.input(files):
             fout.write(line)
 
-def get_page_metadata(source_file_spec, header_regex):
+def get_page_metadata(source_file_spec, header_regex, cdn_url):
     """Build a sorted list of page metadata."""
     page_metadata = []
 
@@ -114,18 +114,18 @@ def get_page_metadata(source_file_spec, header_regex):
         source_filename = os.path.split(path)[1]
         # Replace the .md extension with .html to get the output filename
         output_filename = re.sub(r"(?si)^(.*\.)(md)$", r"\1html", source_filename)
-        markdown_source = codecs.open(path, 'r', 'utf-8')
+        markdown_source = codecs.open(path, "r", "utf-8")
         post_data = parse_page_data(
             header_regex,
             markdown_source.read(),
             source_filename,
             output_filename,
-            CONFIG["cdn2"]
+            cdn_url
         )
         page_metadata.append(post_data)
 
     # Sort the file list by updated date descending
-    return sorted(page_metadata, key=lambda k: k['updated'], reverse=True)
+    return sorted(page_metadata, key=lambda k: k["updated"], reverse=True)
 
 def get_most_recent(count, page_data_list):
     """Get the most recent N items from a list of page metadata."""
@@ -138,17 +138,17 @@ def get_most_recent(count, page_data_list):
 def render_page(template, data, nav_items, config, base_title):
     """Render a page template using the specified data."""
     return template.render(
-        published=data['published'],
-        updated=data['updated'],
-        title=data['title'],
-        permalink=data['output_filename'],
-        body=data['body'],
+        published=data["published"],
+        updated=data["updated"],
+        title=data["title"],
+        permalink=data["output_filename"],
+        body=data["body"],
         nav_items=nav_items,
-        meta_title=data['title'] + base_title,
-        og_title=data['title'],
-        og_abstract=data['abstract_plain'],
-        og_image=data['thumbnail'] if data['thumbnail'] is not None else 'site.png',
-        og_url=data['output_filename'],
+        meta_title=data["title"] + base_title,
+        og_title=data["title"],
+        og_abstract=data["abstract_plain"],
+        og_image=data["thumbnail"] if data["thumbnail"] is not None else "site.png",
+        og_url=data["output_filename"],
         comments=None if is_static_page(data) else 1,
         asset_version=config["asset_version"],
         cdn1=config["cdn1"],
@@ -162,11 +162,11 @@ def render_index(template, data, nav_items, config, base_title):
     return template.render(
         posts=data,
         nav_items=nav_items,
-        meta_title=base_title + ' - C# ASP.NET, jQuery, JavaScript and Python web development',
+        meta_title=base_title + " - C# ASP.NET, jQuery, JavaScript and Python web development",
         og_title=base_title,
-        og_abstract='C# ASP.NET, jQuery, JavaScript and Python web development',
-        og_image='site.png',
-        og_url='',
+        og_abstract="C# ASP.NET, jQuery, JavaScript and Python web development",
+        og_image="site.png",
+        og_url="",
         asset_version=config["asset_version"],
         cdn1=config["cdn1"],
         cdn2=config["cdn2"],
@@ -177,41 +177,41 @@ def render_index(template, data, nav_items, config, base_title):
 HEADER_REGEX_FLAGS = re.IGNORECASE | re.MULTILINE
 
 HEADER_REGEX = {
-    'title': re.compile("(^Title: (.*)[\r\n]+)", HEADER_REGEX_FLAGS),
-    'published': re.compile("(^Published: (.*)[\r\n]+)", HEADER_REGEX_FLAGS),
-    'updated': re.compile("(^Updated: (.*)[\r\n]+)", HEADER_REGEX_FLAGS),
-    'abstract': re.compile("(^Abstract: (.*)[\r\n]+)", HEADER_REGEX_FLAGS),
-    'pagetype': re.compile("(^PageType: (.*)[\r\n]+)", HEADER_REGEX_FLAGS),
-    'thumbnail': re.compile("(^Thumbnail: (.*)[\r\n]+)", HEADER_REGEX_FLAGS)
+    "title": re.compile("(^Title: (.*)[\r\n]+)", HEADER_REGEX_FLAGS),
+    "published": re.compile("(^Published: (.*)[\r\n]+)", HEADER_REGEX_FLAGS),
+    "updated": re.compile("(^Updated: (.*)[\r\n]+)", HEADER_REGEX_FLAGS),
+    "abstract": re.compile("(^Abstract: (.*)[\r\n]+)", HEADER_REGEX_FLAGS),
+    "pagetype": re.compile("(^PageType: (.*)[\r\n]+)", HEADER_REGEX_FLAGS),
+    "thumbnail": re.compile("(^Thumbnail: (.*)[\r\n]+)", HEADER_REGEX_FLAGS)
 }
 
 CONFIG = get_config()
 
 TEMPLATES = load_templates()
 
-delete_files(CONFIG["output_folder"] + '/css/*.css')
-delete_files(CONFIG["output_folder"] + '/js/*.js')
-delete_files(CONFIG["output_folder"] + '/*.html')
+delete_files(CONFIG["output_folder"] + "/css/*.css")
+delete_files(CONFIG["output_folder"] + "/js/*.js")
+delete_files(CONFIG["output_folder"] + "/*.html")
 
-concatenate_files('css/*.min.css', CONFIG["output_folder"] + '/css/all.min.v' + CONFIG["asset_version"] + '.css')
-concatenate_files('js/*.min.js', CONFIG["output_folder"] + '/js/all.min.v' + CONFIG["asset_version"] + '.js')
+concatenate_files("css/*.min.css", CONFIG["output_folder"] + "/css/all.min.v" + CONFIG["asset_version"] + ".css")
+concatenate_files("js/*.min.js", CONFIG["output_folder"] + "/js/all.min.v" + CONFIG["asset_version"] + ".js")
 
-PAGE_DATA_LIST = get_page_metadata('posts/*.md', HEADER_REGEX)
+PAGE_DATA_LIST = get_page_metadata("posts/*.md", HEADER_REGEX, CONFIG["cdn2"])
 
 # Build a list of nav links to individual posts (which are already
-# ordered by created date desc, so we're adding in the correct order)
+# ordered by created date desc, so we"re adding in the correct order)
 NAV_ITEMS = [p for p in PAGE_DATA_LIST if not is_static_page(p)]
 
 for page_data in PAGE_DATA_LIST:
-    base_page_title = ' - Mark Ashley Bell'
+    base_page_title = " - Mark Ashley Bell"
     page_content = render_page(TEMPLATES["post"], page_data, NAV_ITEMS, CONFIG, base_page_title)
-    write_file_utf8(CONFIG["output_folder"] + '/' + page_data['output_filename'], page_content)
+    write_file_utf8(CONFIG["output_folder"] + "/" + page_data["output_filename"], page_content)
 
-BASE_TITLE = 'Mark Ashley Bell, Freelance Web Designer/Developer'
+BASE_TITLE = "Mark Ashley Bell, Freelance Web Designer/Developer"
 
 INDEX = render_index(TEMPLATES["index"], get_most_recent(5, PAGE_DATA_LIST), NAV_ITEMS, CONFIG, BASE_TITLE)
 
-write_file_utf8(CONFIG["output_folder"] + '/index.html', INDEX)
+write_file_utf8(CONFIG["output_folder"] + "/index.html", INDEX)
 
 RSS_XML = create_rss_xml(
     title="Mark Ashley Bell",
@@ -222,6 +222,6 @@ RSS_XML = create_rss_xml(
     items=get_most_recent(10, PAGE_DATA_LIST)
 )
 
-write_file_utf8(CONFIG["output_folder"] + '/rss.xml', RSS_XML)
+write_file_utf8(CONFIG["output_folder"] + "/rss.xml", RSS_XML)
 
-print('File generation complete')
+print("File generation complete")
--- rss.py
+++ rss.py
@@ -14,7 +14,8 @@ def rfc822_date(date):
     """Format a date according to RFC 822"""
     days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
     months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
-    date_data = (
+    date_format = "{:s}, {:02d} {:s} {:04d} {:02d}:{:02d}:{:02d} GMT"
+    return date_format.format(
         days[date.weekday()],
         date.day, months[date.month-1],
         date.year,
@@ -22,7 +23,6 @@ def rfc822_date(date):
         date.minute,
         date.second
     )
-    return "%s, %02d %s %04d %02d:%02d:%02d GMT" % date_data
 
 def create_xml_element(xml, element_name, text=None):
     """Create an XML element containing the specified text."""
@@ -33,22 +33,22 @@ def create_xml_element(xml, element_name, text=None):
 
 def create_rss_item_xml(xml, base_url, item_data):
     """Generate RSS feed item XML from metadata."""
-    item_element = create_xml_element(xml, 'item')
+    item_element = create_xml_element(xml, "item")
 
-    title_element = create_xml_element(xml, 'title', item_data['title'])
-    link_element = create_xml_element(xml, 'link', base_url + '/' + item_data['output_filename'])
+    title_element = create_xml_element(xml, "title", item_data["title"])
+    link_element = create_xml_element(xml, "link", base_url + "/" + item_data["output_filename"])
 
-    description_html = markdown.markdown(item_data['abstract_nolink'], extensions=['extra'])
-    description_element = create_xml_element(xml, 'description', description_html)
+    description_html = markdown.markdown(item_data["abstract_nolink"], extensions=["extra"])
+    description_element = create_xml_element(xml, "description", description_html)
 
-    # Manually updating the 'updated' header value for a post
+    # Manually updating the "updated" header value for a post
     # will cause readers to re-download the item
-    time_stamp = "{0:d}".format(unix_timestamp(item_data['updated']))
-    guid = base_url + '/' + item_data['output_filename'] + '?d=' + time_stamp
-    guid_element = create_xml_element(xml, 'guid', guid)
-    guid_element.setAttribute('isPermaLink', 'false')
+    time_stamp = "{0:d}".format(unix_timestamp(item_data["updated"]))
+    guid = base_url + "/" + item_data["output_filename"] + "?d=" + time_stamp
+    guid_element = create_xml_element(xml, "guid", guid)
+    guid_element.setAttribute("isPermaLink", "false")
 
-    pub_date_element = create_xml_element(xml, 'pubDate', rfc822_date(item_data['updated']))
+    pub_date_element = create_xml_element(xml, "pubDate", rfc822_date(item_data["updated"]))
 
     item_element.appendChild(title_element)
     item_element.appendChild(link_element)
@@ -62,20 +62,20 @@ def create_rss_xml(title, link, description, last_build_date, rss_url, items):
     """Generate RSS feed XML from metadata."""
     xml = Document()
 
-    rss_element = create_xml_element(xml, 'rss')
-    rss_element.setAttribute('version', '2.0')
-    rss_element.setAttribute('xmlns:atom', 'http://www.w3.org/2005/Atom')
+    rss_element = create_xml_element(xml, "rss")
+    rss_element.setAttribute("version", "2.0")
+    rss_element.setAttribute("xmlns:atom", "http://www.w3.org/2005/Atom")
 
-    channel_element = create_xml_element(xml, 'channel')
-    title_element = create_xml_element(xml, 'title', title)
-    link_element = create_xml_element(xml, 'link', link)
-    description_element = create_xml_element(xml, 'description', description)
-    last_build_date_element = create_xml_element(xml, 'lastBuildDate', rfc822_date(last_build_date))
+    channel_element = create_xml_element(xml, "channel")
+    title_element = create_xml_element(xml, "title", title)
+    link_element = create_xml_element(xml, "link", link)
+    description_element = create_xml_element(xml, "description", description)
+    last_build_date_element = create_xml_element(xml, "lastBuildDate", rfc822_date(last_build_date))
 
-    atom_link_element = create_xml_element(xml, 'atom:link')
-    atom_link_element.setAttribute('href', rss_url)
-    atom_link_element.setAttribute('rel', 'self')
-    atom_link_element.setAttribute('type', 'application/rss+xml')
+    atom_link_element = create_xml_element(xml, "atom:link")
+    atom_link_element.setAttribute("href", rss_url)
+    atom_link_element.setAttribute("rel", "self")
+    atom_link_element.setAttribute("type", "application/rss+xml")
 
     channel_element.appendChild(title_element)
     channel_element.appendChild(link_element)
