5364c1b1d894febdc352ff4e1be7c8d540f54f39
Mark Bell <me@markb.co.uk>
2018-01-30 06:42:52 +0000

    Wire up email address substitution

--- js/main.ts
+++ js/main.ts
@@ -1,5 +1,22 @@
-namespace markb.web {
+namespace markb {
+    export function ready(callback: EventListener): void {
+        if (document.readyState === "interactive" || document.readyState === "complete") {
+            callback(new Event("DOMContentLoaded"));
+        } else {
+            document.addEventListener("DOMContentLoaded", callback);
+        }
+    }
+
     export function init(): void {
-        console.log("TEST");
+        let e: HTMLElement = document.getElementById("e");
+
+        if (e) {
+            let mt: string = String.fromCharCode(109, 97, 105, 108, 116, 111, 58);
+            let v: string[] = "109#101#64#109#97#114#107#98#46#99#111#46#117#107".split("#");
+            let t: string = v.map(cc => String.fromCharCode(parseInt(cc, 10))).join("");
+            e.innerHTML = `<a class=\"email-link\" href=\"${mt}${t}\">${t}</a>`;
+        }
     }
 }
+
+markb.ready(markb.init);
