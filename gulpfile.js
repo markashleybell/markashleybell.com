1afcbbd63a7830fbac3e26a849f5947f5d65f4ed
Mark Bell <me@markb.co.uk>
2018-02-01 08:57:22 +0000

    Working CSS workflow (compile SCSS, minify, bundle and create source maps)

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const typescript = require('gulp-typescript');
+const sourcemaps = require('gulp-sourcemaps');
 const cleanCSS = require('gulp-clean-css');
 const uglify = require('gulp-uglify');
 const concat = require('gulp-concat');
 const rename = require('gulp-rename');
+const filter = require('gulp-filter');
 
 const tsProject = typescript.createProject('tsconfig.json');
 
@@ -20,24 +22,22 @@ const output = {
     js: './public/js'
 };
 
-const scss = gulp.src(sources.scss);
-const css = gulp.src(sources.css);
-const ts = gulp.src(sources.ts);
-const js = gulp.src(sources.js);
+const scssFilter = filter('**/*.scss', { restore: true });
 
-const toSameFolder = gulp.dest(filename => filename.base);
+const compileCss = () => {
+    gulp.src(['./css/vendor/*.css', './css/*.scss'])
+    .pipe(sourcemaps.init())
+    .pipe(scssFilter)
+    .pipe(sass())
+    .pipe(scssFilter.restore)
+    .pipe(cleanCSS())
+    .pipe(concat('bundle.css'))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(output.css));
+}
 
-const compileScss = () => scss.pipe(sass()).pipe(toSameFolder);
-const compileTs = () => ts.pipe(tsProject()).pipe(toSameFolder);
+// gulp.task('compile-js', compileJs);
+gulp.task('compile-css', compileCss);
 
-const packCss = () => css.pipe(cleanCSS()).pipe(concat('bundle.css')).pipe(gulp.dest(output.css));
-const packJs = () => js.pipe(uglify()).pipe(concat('bundle.js')).pipe(gulp.dest(output.js));
-
-gulp.task('compile-scss', compileScss);
-gulp.task('compile-typescript', compileTs);
-
-gulp.task('pack-css', ['compile-scss'], packCss);
-gulp.task('pack-js', ['compile-typescript'], packJs);
-
-gulp.task('watch-typescript', () => gulp.watch(sources.ts, ['pack-js']));
-gulp.task('watch-scss', () => gulp.watch(sources.scss, ['pack-css']));
+// gulp.task('watch-js', () => gulp.watch(sources.ts, ['pack-js']));
+// gulp.task('watch-css', () => gulp.watch(sources.scss, ['pack-css']));
